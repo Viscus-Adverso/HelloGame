c9dc3b778b7527d962d228ecbcbd2a234693f5e1
Viscus <keegansaint194@hotmail.com>
2016-09-29 10:32:14 -0400

    added movement code.

--- core/src/com/theironyard/MyGdxGame.java
+++ core/src/com/theironyard/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.theironyard;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,7 +10,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	float x, y, xv, yv;
+
+	static final float MAX_VELOCITY = 100;
+	static final float FRICTION = 0.9f;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -18,10 +23,14 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		move();
+
+		System.out.println(xv + " " + yv);
+
+		Gdx.gl.glClearColor(255, 0, 128, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	
@@ -30,4 +39,34 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+
+	public void move() {
+		if (Gdx.input.isKeyPressed(Input.Keys.W)){
+			yv = MAX_VELOCITY;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.S)){
+			yv = MAX_VELOCITY * -1;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+			xv = MAX_VELOCITY;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+			xv = MAX_VELOCITY * -1;
+		}
+
+		x += xv * Gdx.graphics.getDeltaTime();
+		y += yv * Gdx.graphics.getDeltaTime();
+
+		xv = decelerate(xv);
+		yv = decelerate(yv);
+	}
+
+	public float decelerate(float velocity) {
+		velocity *= FRICTION;
+		if (Math.abs(velocity) <1) {
+			velocity = 0;
+		}
+		return velocity;
+	}
+
 }
